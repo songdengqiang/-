8952e48ba3c1ee6fc6b08c7c90535f9c134a4846
songdengqiang <ai491154541@163.com>
2020-08-05 13:46:43 +0800

    实时监控页面的修改1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,25 +52,73 @@
           <div></div>
         </div>
       </div>
-      <div class="topBar1">
+      <div class="topBar topBar1">
         <div class="btns">
           <div class="caidan">实时监控</div>
-          <div>
-            <a href="#">监控区域1</a>
-            <a href="#">监控区域2</a>
-            <a href="#">监控区域3</a>
-          </div>
         </div>
         <div class="btns">设备信息</div>
         <div class="btns">仓储信息</div>
       </div>
-      <div class="topBar2">
+      <div class="topBar topBar2">
         <div class="btns">故障信息</div>
         <div class="btns">维修记录</div>
         <div class="btns">系统设置</div>
       </div>
     </header>
     <main class="contentInfo">
+      <div class="leftBar">
+        <div>
+          <table>
+            <tr>
+              <th>基础信息</th>
+            </tr>
+            <tr>
+              <td>纺丝产线</td>
+              <td>L1</td>
+            </tr>
+            <tr>
+              <td>纺丝位号</td>
+              <td>21</td>
+            </tr>
+            <tr>
+              <td>纺丝速度(m/min)</td>
+              <td>3160</td>
+            </tr>
+            <tr>
+              <td>剩余时间(sec)</td>
+              <td>8678</td>
+            </tr>
+            <tr>
+              <td>当前卷径(mm)</td>
+              <td>319</td>
+            </tr>
+            <tr>
+              <td>络筒时间</td>
+              <td>16:02:54</td>
+            </tr>
+            <tr>
+              <td>丝线品种</td>
+              <td>经纶 0.05</td>
+            </tr>
+          </table>
+        </div>
+        <div></div>
+      </div>
+      <div class="centerBar">
+        <div class="headerInfo">
+          <div class="titleIcon">
+            <img src="../assets/img/设备监控.svg" alt="图标">
+            <div>设备的选择</div>
+          </div>
+          <div>Map OverView</div>
+          <div>设备介绍</div>
+        </div>
+        <div class="deviceImg">
+          <img src="../assets/img/device.jpg" alt="设备">
+        </div>
+        <div></div>
+      </div>
+      <div class="rightBar"></div>
     </main>
     <footer class="footerCss">
       <h6>系统的设置</h6>
@@ -113,13 +161,18 @@ export default {
 </script>
 
 <style scoped>
+/* 头部样式的设置 */
   @import url("../assets/Css/home/header.css");
   @import url("../assets/Css/home/footer.css");
-  @import url("../assets/Css/home/main.css");
   @import url("../assets/Css/common/decoration.css");
   @import url("../assets/Css/common/decoration1.css");
   @import url("../assets/Css/common/btn1.css");
+/* 主体内容的设置 */
+  @import url("../assets/Css/home/main.css");
 
+/* 尾部样式的设置 */
+
+/* 全局样式的设置 */
   *{
     padding: 0;
     margin: 0;
